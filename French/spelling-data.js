f385570a45e42a7d0cffba6de79afeb6821e695e
Mario Ferreira <zeroferreira@MacBook-Pro-de-Zero.local>
2026-03-07 21:04:57 -0600

    feat: add missing data files for French and German

---
+++ French/spelling-data.js
@@ -0,0 +1,530 @@
+window.SPELLING_DATA = {
+  "language": "fr-FR",
+  "levelAWords": [
+    {
+      "word": "adolescence",
+      "definition": "Adolescencia.",
+      "example": "L'adolescence, c'est une époque difficile. "
+    },
+    {
+      "word": "curieux",
+      "definition": "curioso",
+      "example": "C'est un chien curieux."
+    },
+    {
+      "word": "enfance",
+      "definition": "Tiempo en el que transcurre la infancia ",
+      "example": "Mon enfance était plus belle."
+    },
+    {
+      "word": "gare",
+      "definition": "Estación de transporte ",
+      "example": "Je vais à la gare pour prendre le train."
+    },
+    {
+      "word": "pyramide",
+      "definition": "Piramide ",
+      "example": "Les égyptiens enterraient leurs pharaons dans des pyramides."
+    },
+    {
+      "word": "communauté",
+      "definition": "comunidad",
+      "example": "Nous faisons partie de la communauté française de ce pays."
+    },
+    {
+      "word": "courageuse",
+      "definition": "Valiente",
+      "example": "Il est courageux, il se lève tous les jours à 6h."
+    },
+    {
+      "word": "gouvernement",
+      "definition": "gobierno",
+      "example": "Nous detestons le gouvernement actuelle"
+    },
+    {
+      "word": "malheur",
+      "definition": "Desgracia",
+      "example": "Si tu es dans le malheur, tu peux compter sur tes amis."
+    },
+    {
+      "word": "sorcière",
+      "definition": "Bruja, hechicera",
+      "example": "La sorcière porte un chapeau noir"
+    },
+    {
+      "word": "indépendant",
+      "definition": "Independiente",
+      "example": "Le Mexique  est devenue indépendant en 1810"
+    },
+    {
+      "word": "drôle",
+      "definition": "gracioso",
+      "example": "mon ami, est très drôle"
+    },
+    {
+      "word": "valeur",
+      "definition": "Valor",
+      "example": "La valeur de l'or augmente en ce moment."
+    },
+    {
+      "word": "pharmacie",
+      "definition": "Farmacia",
+      "example": "je travaille dans une pharmacie"
+    },
+    {
+      "word": "étranger",
+      "definition": "Extranjero, foranéo",
+      "example": "Il y a un étudiant étranger dans ma classe."
+    },
+    {
+      "word": "secteur",
+      "definition": "Sector",
+      "example": "Luc travaille dans le secteur du commerce international."
+    },
+    {
+      "word": "stage",
+      "definition": "prácticas, curso, entrenamiento",
+      "example": "Tous les étudiants devront effectuer un stage chez un professionnel à la fin de l'année."
+    },
+    {
+      "word": "naissance",
+      "definition": "nacimiento",
+      "example": "La naissance d'un enfant est une grande joie."
+    },
+    {
+      "word": "espoir",
+      "definition": "esperanza",
+      "example": "Ce prisonnier a toujours gardé l'espoir  en sa libération."
+    },
+    {
+      "word": "finir",
+      "definition": "acabar, terminar, finalizar",
+      "example": "Elle va finir son dévoir aujourd'hui"
+    },
+    {
+      "word": "travail",
+      "definition": "Trabajo",
+      "example": "Le travail de mon père est très dur"
+    },
+    {
+      "word": "biscuit",
+      "definition": "galleta",
+      "example": "On peut accompagner le café avec de petits biscuits au citron."
+    },
+    {
+      "word": "beaucoup",
+      "definition": "mucho, demasiado, basto.",
+      "example": "Je t'aime beaucoup"
+    },
+    {
+      "word": "chercher",
+      "definition": "buscar",
+      "example": "Tu es en train de chercher ton chien?"
+    },
+    {
+      "word": "bibliothèque",
+      "definition": "biblioteca",
+      "example": "Oú se trouve la bibliothéque?"
+    },
+    {
+      "word": "bienvenue",
+      "definition": "Bienvenido",
+      "example": "Bienvenue chez moi"
+    },
+    {
+      "word": "oiseaux",
+      "definition": "pájaros",
+      "example": "Les cinq oiseaux sont beaux."
+    },
+    {
+      "word": "boire",
+      "definition": "Bebida ",
+      "example": "Que est-ce  que vous voulez boire?"
+    },
+    {
+      "word": "délicieux",
+      "definition": "Delicioso",
+      "example": "L'hôtel propose un délicieux petit déjeuner "
+    },
+    {
+      "word": "chemisier",
+      "definition": "blusa",
+      "example": "Elle portait un chemisier rouge."
+    },
+    {
+      "word": "ingénieur",
+      "definition": "ingeniero",
+      "example": "Il est ingénieur"
+    },
+    {
+      "word": "chaussures",
+      "definition": "zapatos",
+      "example": "Mes nouvelles chaussures sont confortables."
+    },
+    {
+      "word": "réussir",
+      "definition": "Lograr",
+      "example": "Je te souhaite de réussir."
+    },
+    {
+      "word": "bijoux",
+      "definition": "Joyas",
+      "example": "Ma femme aime les bijoux "
+    },
+    {
+      "word": "voyager",
+      "definition": "Viajes ",
+      "example": "Voyager pour le monde, c'est une plasier."
+    },
+    {
+      "word": "lunettes",
+      "definition": "Lentes ",
+      "example": "Elle est blonde est elle porte des lunettes"
+    },
+    {
+      "word": "moutarde",
+      "definition": "mostaza",
+      "example": "J'aime l'hamburguer avec moutarde"
+    },
+    {
+      "word": "imperméable",
+      "definition": "impermeable",
+      "example": "N'oubliez pas l'imperméable pour la pluie."
+    },
+    {
+      "word": "crocodile",
+      "definition": "cocodrilo",
+      "example": "Le cocodrile est grand et vert."
+    },
+    {
+      "word": "mignonne",
+      "definition": "femenino: linda, bella, bonita",
+      "example": "La petite fille était vraiment mignonne avec son sourire éclatant."
+    },
+    {
+      "word": "chevalier",
+      "definition": "caballero",
+      "example": "Le chevalier courageux brandit son épée pour protéger le royaume."
+    },
+    {
+      "word": "événement",
+      "definition": "evento",
+      "example": "L'événement du français est aujourd'hui"
+    },
+    {
+      "word": "printemps",
+      "definition": "primavera",
+      "example": "Les fleurs commencent à éclore, signe que le printemps est arrivé."
+    },
+    {
+      "word": "grenouille",
+      "definition": "rana",
+      "example": "La grenouille sauta joyeusement dans l'étang."
+    },
+    {
+      "word": "yeux",
+      "definition": "ojos",
+      "example": "Ses yeux brillaient d'excitation quand elle ouvrit son cadeau d'anniversaire."
+    },
+    {
+      "word": "poisson",
+      "definition": "pez, pescado",
+      "example": "Nous avons dégusté un délicieux poisson grillé au dîner."
+    },
+    {
+      "word": "descendre",
+      "definition": "decender, bajar",
+      "example": "Nous avons dû descendre plusieurs étages pour rejoindre la sortie."
+    },
+    {
+      "word": "famille ",
+      "definition": "familia",
+      "example": "Les réunions de famille sont toujours joyeuses et animées."
+    },
+    {
+      "word": "beau ",
+      "definition": "bonito, bello, hermoso",
+      "example": "C'était une journée ensoleillée avec un beau ciel bleu."
+    },
+    {
+      "word": "belle ",
+      "definition": "guapa, bella, hermosa",
+      "example": "La princesse est belle."
+    },
+    {
+      "word": "frère",
+      "definition": "hermano",
+      "example": "Mon frère cadet est très doué en mathématiques."
+    },
+    {
+      "word": "cousin",
+      "definition": "primo, prima",
+      "example": "Mon cousin vient souvent nous rendre visite pendant les vacances."
+    },
+    {
+      "word": "tante",
+      "definition": "tía",
+      "example": "Ma tante prépare toujours les meilleurs gâteaux."
+    },
+    {
+      "word": "oncle",
+      "definition": "tío",
+      "example": "Mon oncle est un grand voyageur qui a visité de nombreux pays."
+    },
+    {
+      "word": "parents",
+      "definition": "padres",
+      "example": "Mes parents sont fiers de mes réalisations académiques."
+    },
+    {
+      "word": "architecte",
+      "definition": "arquitecto, arquitecta",
+      "example": "L'architecte a conçu un bâtiment moderne et écologique."
+    },
+    {
+      "word": "généreux",
+      "definition": "generoso",
+      "example": "Il est connu pour être un homme généreux qui aide toujours les autres."
+    },
+    {
+      "word": "murmurer",
+      "definition": "murmurar",
+      "example": "Les feuilles des arbres murmuraient doucement dans le vent."
+    },
+    {
+      "word": "individualisme",
+      "definition": "individualismo",
+      "example": "Notre société valorise souvent l'individualisme au détriment du bien commun."
+    },
+    {
+      "word": "noble",
+      "definition": "noble",
+      "example": "Le roi était un homme noble qui se souciait du bien-être de son peuple."
+    },
+    {
+      "word": "bateau",
+      "definition": "Bote",
+      "example": "Nous avons fait une promenade en bateau sur le lac au coucher du soleil."
+    },
+    {
+      "word": "projet",
+      "definition": "Proyecto",
+      "example": "Nous avons présenté notre projet lors de la réunion d'équipe."
+    },
+    {
+      "word": "mannequin",
+      "definition": "Modelo",
+      "example": "Elle rêvait de devenir mannequin depuis son plus jeune âge."
+    },
+    {
+      "word": "oignon",
+      "definition": "Cebolla",
+      "example": "J'ai coupé un oignon pour préparer une soupe savoureuse."
+    },
+    {
+      "word": "psychologue",
+      "definition": "Psicólogo",
+      "example": "Mon ami consulte un psychologue pour surmonter ses angoisses."
+    },
+    {
+      "word": "papillon",
+      "definition": "Mariposa",
+      "example": "Les papillons voltigeaient autour des fleurs dans le jardin."
+    },
+    {
+      "word": "saucisson",
+      "definition": "Salchicha",
+      "example": "Nous avons acheté du saucisson artisanal pour l'apéritif."
+    },
+    {
+      "word": "chaussettes",
+      "definition": "Medias",
+      "example": "Elle a mis ses chaussettes chaudes avant de sortir dans la neige."
+    },
+    {
+      "word": "musique",
+      "definition": "música",
+      "example": "J'adore écouter de la musique"
+    },
+    {
+      "word": "orthographe",
+      "definition": "Ortografía",
+      "example": "L'orthographe correcte est importante pour une communication claire."
+    },
+    {
+      "word": "baguette",
+      "definition": "Baguette",
+      "example": "J'ai acheté une baguette fraîche à la boulangerie ce matin."
+    },
+    {
+      "word": "passeport",
+      "definition": "Pasaporte",
+      "example": "Assurez-vous d'avoir votre passeport valide avant de partir en voyage."
+    },
+    {
+      "word": "voleur",
+      "definition": "ladrón",
+      "example": "Le voleur a été arrêtée par la police après avoir essayé de voler un bijou."
+    },
+    {
+      "word": "incroyable",
+      "definition": "increíble",
+      "example": "C'était une performance incroyable qui a émerveillé le public."
+    },
+    {
+      "word": "ville",
+      "definition": "Ciudad",
+      "example": "J'adore me promener dans les rues de la ville."
+    },
+    {
+      "word": "meilleur",
+      "definition": "Mejor",
+      "example": "Il est toujours déterminé à faire de son mieux et à être le meilleur."
+    },
+    {
+      "word": "orange",
+      "definition": "naranja",
+      "example": "mon fruit préféré est l'orange"
+    },
+    {
+      "word": "jaune",
+      "definition": "amarillo",
+      "example": "Les bananes sont jaunes."
+    },
+    {
+      "word": "intelligent",
+      "definition": "inteligente",
+      "example": "Mon ami est très intelligent"
+    },
+    {
+      "word": "cours",
+      "definition": "clase",
+      "example": "Le cours de français est très difficile"
+    },
+    {
+      "word": "méchant",
+      "definition": "malvado",
+      "example": "le prof du français est méchant"
+    },
+    {
+      "word": "dentiste",
+      "definition": "dentista",
+      "example": "mon dentiste est sympa"
+    },
+    {
+      "word": "roi",
+      "definition": "rey",
+      "example": "le roi de l'Anglaterre est vieux"
+    },
+    {
+      "word": "vieux",
+      "definition": "viejo",
+      "example": "j'ai un vieux portable"
+    },
+    {
+      "word": "liberté",
+      "definition": "libertad",
+      "example": "la liberté est un droit humain"
+    },
+    {
+      "word": "appétit",
+      "definition": "apetito, hambre",
+      "example": "Il a bon appétit, c'est bon signe."
+    },
+    {
+      "word": "architecte",
+      "definition": "arquitecto ",
+      "example": "Mon père est architecte"
+    },
+    {
+      "word": "vélo",
+      "definition": "bicicleta",
+      "example": "Je vais acheter un vélo"
+    },
+    {
+      "word": "cadeau",
+      "definition": "regalo",
+      "example": "J'ai acheté un cadeau pour ma soeur "
+    },
+    {
+      "word": "calendrier",
+      "definition": "calendario",
+      "example": "Dans le calendrier vous voyez les jours de l’année"
+    },
+    {
+      "word": "cheval",
+      "definition": "caballo",
+      "example": "Mon ami a un cheval"
+    },
+    {
+      "word": "champignons",
+      "definition": "champiñones",
+      "example": "Les schtroumpfs vivent dans des petits champignons"
+    },
+    {
+      "word": "boulangerie",
+      "definition": "panadería",
+      "example": "Quand je veux acheter du pain, je vais à la boulangerie"
+    },
+    {
+      "word": "coiffeur",
+      "definition": "peluquero",
+      "example": "Je me coupe le cheveux avec le coiffeur"
+    },
+    {
+      "word": "dauphin",
+      "definition": "delfín",
+      "example": "Les dauphins accompagnent souvent les voiliers."
+    },
+    {
+      "word": "embouteillage",
+      "definition": "embotellamiento",
+      "example": "À cause de l'accident il y a un embouteillage qui empêche la circulation normale sur la route."
+    },
+    {
+      "word": "canapé",
+      "definition": "sofá",
+      "example": "Le garçon est assis dans le canapé vert"
+    },
+    {
+      "word": "mammifère",
+      "definition": "mamífero",
+      "example": "L'homme est un mammifère bipède."
+    },
+    {
+      "word": "monnaie",
+      "definition": "moneda",
+      "example": "Pardon, je n'ai pas de monnaie, je n'ai qu'un billet de 20 euros."
+    },
+    {
+      "word": "parapluie",
+      "definition": "paraguas",
+      "example": "N'oublie pas ton parapluie, il va pleuvoir !"
+    },
+    {
+      "word": "pâtisserie",
+      "definition": "pastelería ",
+      "example": "Je suis allée à la pâtisserie"
+    },
+    {
+      "word": "silhouette",
+      "definition": "silueta",
+      "example": "J'ai vu une silhouette à l'extérieur de la maison"
+    },
+    {
+      "word": "tabouret",
+      "definition": "Banco/silla",
+      "example": "Le tabouret de la cuisine est marron"
+    },
+    {
+      "word": "téléphone",
+      "definition": "teléfono",
+      "example": "je vais changer mon numéro de téléphone"
+    },
+    {
+      "word": "vaisselle",
+      "definition": "Platos",
+      "example": "Elle lave la vaiselle"
+    }
+  ]
+};
